c78ff20e8b95eb6da475da8f99839fe318a50d65
m7md3b <76055341+m7md3b@users.noreply.github.com>
2022-05-02 23:46:29 +0300

    MarkdownV2
    Start using telegram MarckdownV2 by escape and replace only  to make the message author in *BOLD*.
    Added message link button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// discord client
 const { Client, Intents} = require("djs-selfbot");
 const SelfDiscord = require("djs-selfbot");
 const Userclient = new SelfDiscord.Client({ intents: [
@@ -11,26 +12,67 @@ const Userclient = new SelfDiscord.Client({ intents: [
 partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
-const config = require('./config.json');
+// config file
+const config = require('./config.json'); 
+
+// telegraf package
 const { Telegraf } = require('telegraf');
 const TelApp = new Telegraf(config.TelegramToken);
 
 Userclient.on('ready', () => {
 	console.log('UserCliant is Ready');
-	Userclient.user.setStatus('invisible');
+	Userclient.user.setStatus('invisible'); // online, idle, invisible, dnd
 });
-
+ 
 Userclient.on("messageCreate", (message) => {
     if (message.channel.type == 'DM') {  
-        if (message.author.id === (Userclient.user.id)) return;     
-		UserclientTelApp.telegram.sendMessage(config.TelegramDMsChannel, `${message.author.tag}: ${message.content}`)
-		.catch(error => {console.error(error);});       
+        if (message.author.id === (Userclient.user.id)) return;    	
+        
+        let mContent = message.content.replace(/\_/g, '\\_').replace(/\#/g, '\\#').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\(/g, '\\(')
+            .replace(/\)/g, '\\)').replace(/\~/g, '\\~').replace(/\`/g, '\\`').replace(/\>/g, '\\>').replace(/\+/g, '\\+').replace(/\-/g, '\\-').replace(/\=/g, '\\=')
+            .replace(/\|/g, '\\|').replace(/\{/g, '\\{').replace(/\}/g, '\\}').replace(/\./g, '\\.').replace(/\!/g, '\\!')
+
+        let mAuthor = message.author.tag.replace(/\_/g, '\\_').replace(/\#/g, '\\#').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\(/g, '\\(')
+            .replace(/\)/g, '\\)').replace(/\~/g, '\\~').replace(/\`/g, '\\`').replace(/\>/g, '\\>').replace(/\+/g, '\\+').replace(/\-/g, '\\-').replace(/\=/g, '\\=')
+            .replace(/\|/g, '\\|').replace(/\{/g, '\\{').replace(/\}/g, '\\}').replace(/\./g, '\\.').replace(/\!/g, '\\!');
+
+        UserclientTelApp.telegram.sendMessage(config.TelegramDMsChannel, `*${mAuthor}:* ${mContent}`,
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [ { text: "🍁", url: message.url } ]
+                    ]
+                }, 
+                parse_mode: 'MarkdownV2'
+            })
+            .catch(error => {console.error(error);});       
     }
+
     if (message.content.includes(Userclient.user.id)){
-		let mContent = message.content;
-  if (mContent.includes(Userclient.user.id)) mContent = mContent.replace(Userclient.user.id, '3ba')
-        UserclientTelApp.telegram.sendMessage(config.TelegramMentionsChannel, `${message.author.tag} - ${message.guild.name}: ${mContent}`)
-		.catch(error => {console.error(error);}); 
+
+    let mContent = message.content.replace(/\_/g, '\\_').replace(/\#/g, '\\#').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\(/g, '\\(')
+        .replace(/\)/g, '\\)').replace(/\~/g, '\\~').replace(/\`/g, '\\`').replace(/\>/g, '\\>').replace(/\+/g, '\\+').replace(/\-/g, '\\-').replace(/\=/g, '\\=')
+        .replace(/\|/g, '\\|').replace(/\{/g, '\\{').replace(/\}/g, '\\}').replace(/\./g, '\\.').replace(/\!/g, '\\!')
+        .replace(`<@${Userclient.user.id}\\>`, `*${Userclient.user.username}*`).replace(`<@!${Userclient.user.id}\\>`, `*${Userclient.user.username}*`);
+
+    let mAuthor = message.author.tag.replace(/\_/g, '\\_').replace(/\#/g, '\\#').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\(/g, '\\(')
+        .replace(/\)/g, '\\)').replace(/\~/g, '\\~').replace(/\`/g, '\\`').replace(/\>/g, '\\>').replace(/\+/g, '\\+').replace(/\-/g, '\\-').replace(/\=/g, '\\=')
+        .replace(/\|/g, '\\|').replace(/\{/g, '\\{').replace(/\}/g, '\\}').replace(/\./g, '\\.').replace(/\!/g, '\\!');
+    
+    let mGuild = message.guild.name.replace(/\_/g, '\\_').replace(/\#/g, '\\#').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\(/g, '\\(')
+        .replace(/\)/g, '\\)').replace(/\~/g, '\\~').replace(/\`/g, '\\`').replace(/\>/g, '\\>').replace(/\+/g, '\\+').replace(/\-/g, '\\-').replace(/\=/g, '\\=')
+        .replace(/\|/g, '\\|').replace(/\{/g, '\\{').replace(/\}/g, '\\}').replace(/\./g, '\\.').replace(/\!/g, '\\!');
+
+        UserclientTelApp.telegram.sendMessage(config.TelegramMentionsChannel, `*${mAuthor}* \\- ${mGuild}: ${mContent}`,
+           {
+                reply_markup: {
+                    inline_keyboard: [
+                        [ { text: "🍁", url: message.url } ]
+                    ]
+                }, 
+                parse_mode: 'MarkdownV2'
+            })
+            .catch(error => {console.error(error);}); 
     }
 });
 
